f7785c3b35cdb9cd0b3d38c90fd29d5761f23b6b
Christian Gunderman <gundermanc@gmail.com>
2014-03-27 20:37:23 -0400

    Commented Gunderscript.c

--- src/gunderscript.c
+++ src/gunderscript.c
@@ -26,6 +26,21 @@
 
 #include "gunderscript.h"
 
+/**
+ * Creates a new instance of Gunderscript object with a compiler and 
+ * a Virtual Machine.
+ * instance: pointer to a Gunderscript object that will receive
+ * the instance.
+ * stackSize: the size for the VM stack in bytes. The VM does not
+ * dynamically resize so this value is absolute for this instance.
+ * callbacksSize: the number of callbacks slots. This number defines
+ * how many native functions can be bound to this instance. Increase
+ * this value if vm_reg_callback() fails, or if gunderscript_new()
+ * always returns false.
+ * returns: true if creation succeeds, and false if fails. Failure can
+ * occur due to malloc failure or if callbacksSize is too small to
+ * contain all of the standard libraries.
+ */
 bool gunderscript_new(Gunderscript * instance, size_t stackSize,
 		      int callbacksSize) {
   assert(instance != NULL);
@@ -56,16 +71,36 @@ bool gunderscript_new(Gunderscript * instance, size_t stackSize,
   return true;
 }
 
+/**
+ * Gets the instance of compiler for this instance of Gunderscript,
+ * or NULL if this instance does not have a Compiler.
+ * instance: an instance of Gunderscript.
+ * returns: an instance of Compiler.
+ */
 Compiler * gunderscript_compiler(Gunderscript * instance) {
   assert(instance != NULL);
   return instance->compiler;
 }
 
+/**
+ * Gets the instance of Virtual Machine for this instance of Gunderscript.
+ * instance: an instance of Gunderscript.
+ * returns: an instance of VM.
+ */
 VM * gunderscript_vm(Gunderscript * instance) {
   assert(instance != NULL);
   return instance->vm;
 }
 
+/**
+ * Builds a gunderscript and appends its OPCode to the buffer of OP data for
+ * any previously built files. You can build multiple files as long as the 
+ * dependencies come first.
+ * instance: an instance of Gunderscript.
+ * input: a pointer to a buffer of Gunderscript code.
+ * inputLen: the length of the input.
+ * returns: an instance of VM.
+ */
 bool gunderscript_build(Gunderscript * instance, char * input, size_t inputLen) {
   assert(instance != NULL);
   assert(input != NULL);
@@ -73,11 +108,21 @@ bool gunderscript_build(Gunderscript * instance, char * input, size_t inputLen) 
   return compiler_build(instance->compiler, input, inputLen);
 }
 
-bool gunderscript_build_err(Gunderscript * instance) {
+/**
+ * Gets any compiler errors that may have occurred.
+ * instance: an instance of Gunderscript.
+ * returns: a CompilerErr, defined in compiler.h.
+ */
+CompilerErr gunderscript_build_err(Gunderscript * instance) {
   assert(instance != NULL);
   return compiler_get_err(instance->compiler);
 }
 
+/**
+ * Gets a textual error message representing the last error, if there is one.
+ * instance: an instance of Gunderscript.
+ * returns: an error message, or NULL if there are no errors.
+ */
 char * gunderscript_err_message(Gunderscript * instance) {
   assert(instance != NULL);
 
@@ -93,11 +138,23 @@ char * gunderscript_err_message(Gunderscript * instance) {
   }
 }
 
+/**
+ * Gets the line where the last known compiler error occurred.
+ * instance: an instance of Gunderscript.
+ * returns: a line number.
+ */
 int gunderscript_err_line(Gunderscript * instance) {
   assert(instance != NULL);
   return compiler_err_line(instance->compiler);
 }
 
+/**
+ * Runs the specified Gunderscript "exported" function.
+ * instance: an instance of Gunderscript.
+ * entryPoint: the name of an entryPoint function to run.
+ * entryPointLen: the length of entryPoint in chars.
+ * returns: true if a success, and false if an error occurs.
+ */
 bool gunderscript_function(Gunderscript * instance, char * entryPoint,
 			   size_t entryPointLen) {
   CompilerFunc * function;
@@ -118,11 +175,21 @@ bool gunderscript_function(Gunderscript * instance, char * entryPoint,
   return true;
 }
 
-bool gunderscript_function_err(Gunderscript * instance) {
+/**
+ * Gets the error that occurred during the last call to gunderscript_function.
+ * instance: an instance of Gunderscript.
+ * returns: a VMErr
+ */
+VMErr gunderscript_function_err(Gunderscript * instance) {
   assert(instance != NULL);
   return vm_get_err(instance->vm);
 }
 
+/**
+ * Frees a Gunderscript object.
+ * instance: an instance of Gunderscript.
+ * returns: a VMErr
+ */
 void gunderscript_free(Gunderscript * instance) {
   assert(instance != NULL);
   compiler_free(instance->compiler);
