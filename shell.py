ea029dbf448206bc8fc4b9afccb838f1fb0af1b1
Kai Smith <KaiSmith3027@gmail.com>
2014-04-04 04:10:36 -0400

    Added capability for functions and depends statements (when they are added). Also improved printing method

--- shell.py
+++ shell.py
@@ -6,36 +6,53 @@ email: kjs108@case.edu
 from subprocess import Popen, PIPE
 import readline
 
-def run(program, program_name, printout):
+def assemble(depends, functions, program):
+    p = "\n".join(depends)+"\n"
+    p+= "\n".join(sum(functions, []))+"\n"
+    p+= "\n".join(sum(program, []))+"\n}"
+    return p
+
+def run(program, program_name, prevout):
     program_file = open(program_name, "w")
     program_file.write(program)
     program_file.close()
     output = Popen(["./gunderscript", "main", program_name], stdout = PIPE).stdout.read()
+    realout = "\n".join(output.split("\n")[3:-2])
+    if prevout == realout[:len(prevout)]:
+        realout = realout[len(prevout):]
     if "Compiler Error" in output:
         print("\n".join(output.split("\n")[5:]))
-        return False
-    if printout:
-        print("\n".join(output.split("\n")[3:-2]))
-    return True
+        return (False, False)
+    elif len(realout) > 0:
+        print(realout)
+    return (True, "\n".join(output.split("\n")[3:-2]))
 
 if __name__ == "__main__":
     program_name = "shell_prog.gxs"
     program = [["function exported main(){"]]
+    outlines = ""
     openparens = 0
     openbrackets = 0
     openbraces = 0
     tabwidth = 2
     inblock = False
+    funcdef = False
+    depends= []
+    functions = []
     block = []
     while True:
         if not inblock:
             i = raw_input(">>> "+tabwidth*(openparens+openbrackets+openbraces)*" ")
         else:
             i = raw_input("... "+tabwidth*(openparens+openbrackets+openbraces)*" ")
+        if i[:9].lower() == "function " and block == []:
+            funcdef = True
+        if i[:8].lower() == "depends ":
+            depends.append(i)
         if i.lower() == "end" and not inblock:
             break
         elif i.lower() == "run" and not inblock:
-            run("\n".join(sum(program, []))+"\n}", program_name)
+            run(assemble(depends, functions, program), program_name, outlines)
         elif i.lower() == "view block":
             print("\n".join(block))
         elif i.lower() == "reset block":
@@ -47,7 +64,7 @@ if __name__ == "__main__":
         elif i[:5].lower() == "save ":
             try:
                 save_file = open(i[5:], "w")
-                save_file.write("\n".join(sum(program, []))+"\n}")
+                save_file.write(assemble(depends, functions, program))
                 save_file.close()
             except:
                 print("Error saving. Likely an invalid file name.")
@@ -67,19 +84,26 @@ if __name__ == "__main__":
             else:
                 inblock = False
                 block.append(i)
-                program.append(block)
-                success = run("\n".join(sum(program, []))+"\n}", program_name, "sys_print(" in "".join(block))
+                if funcdef:
+                    functions.append(block)
+                else:
+                    program.append(block)
+                results = run(assemble(depends, functions, program), program_name, outlines)
+                success = results[0]
+                if results[1] != False:
+                    outlines = results[1]
                 if not success:
                     block = []
                     inblock = False
                     openparens = 0
                     openbrackets = 0
                     openbraces = 0
-                    program = program[:-1]
+                    if funcdef:
+                        functions = functions[:-1]
+                    else:
+                        program = program[:-1]
                 else:
                     block = []
-                    for n, line in enumerate(program[-1]):
-                        if "sys_print(" in line:
-                            program[-1][n] = "//"+line
+                funcdef = False
 
     Popen(["rm", program_name])
